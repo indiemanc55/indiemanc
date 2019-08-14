cac2b288a3f22821200965c1a814715edfb49823
codenation <codenation@codenations-MacBook-Pro-11.local>
2019-08-14 17:28:20 +0100

    functions

--- staqpletomn.js
+++ staqpletomn.js
@@ -1,3 +1,52 @@
-console.log ("Hello") ;
+/*console.log ("Hello") ;
+console.log( const takeOrder = (size, drinkType) => {
+    console.log (`Order received: ${size} $(drinkType)
+    `)
+function square (n) { if (( n===0 || (n ===1)) 
+    const square = function (number) { return number * number} 
+}; 
+square (5);
+
+// Output : 25
+
+*/
+//let orderCount = 0;
+//const takeOrder = (topping1, topping2, topping3) => {
+ //console.log(`Pizza with ${topping1}, ${topping2},${topping3}. Order number: 
+// orderCount
+ //return (orderCount++)
+//}
+//takeOrder("cheese", "tomato", "spinach";
+
+//let orderCount = 0;
+/*
+var pin = (1984)
+var avBal = (124.95)
+let (withdraw) = 50
+var access = (true)
+let pinT = (1984)
+if pinT === pin){
+    access = true
+    console.log (Your balance is $(avBalance)
+
+    else {
+        access = (false)
+        console.log ('Pin Incorrect')
+        
+        var access
+        if (withdraw > avBalance) {
+          console.log (no go)  
+        }
+        
+        access false 
 
 
+}
+*/
+
+const cashMachine = (pin, amount) => {
+    if (pin = 1234 && amount <= 1000) {
+        console.log("Take your money")
+    }
+}
+cashMachine (1234, 900)
