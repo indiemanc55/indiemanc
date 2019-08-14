f0e680456911b872f2b984b1692de43f5f64e49e
codenation <codenation@codenations-MacBook-Pro-11.local>
2019-08-14 14:07:55 +0100

    My first commit

---
+++ dayone.js
@@ -0,0 +1,15 @@
+//This is a comment for a loop
+
+// (i = 0; i < 10; i++ ){
+//     console.log(Hello.length
+
+//console.log("hello".toUpperCase())
+//console.log("Hello"    .trim
+//console.log("Hello".ToLowerCase())
+//console.log(Math.floor(Math.random() * 10))
+//var
+//console.log("All Around The World".toUpperCase().charAt(7));
+let favouriteDrink = "coffee";
+//console.log(favouriteDrink);
+console.log(`My favourite drink is ${favouriteDrink}`);
+
---
+++ dayone1.js
@@ -0,0 +1,68 @@
+//This is a comment for a loop
+
+// (i = 0; i < 10; i++ ){
+//     console.log(Hello.length
+
+//console.log("hello".toUpperCase())
+//console.log("Hello"    .trim
+//console.log("Hello".ToLowerCase())
+//console.log(Math.floor(Math.random() * 10))
+//var
+//console.log("All Around The World".toUpperCase().charAt(7));
+let favouriteDrink = "coffee";
+//console.log(favouriteDrink);
+console.log(`My favourite drink is ${favouriteDrink}`);
+favouriteDrink = "icedCoffee";
+
+console.log(`My favourite drink is now ${favouriteDrink}`);
+favouriteDrink = "tea";
+
+console.log(`My favourite drink is now ${favouriteDrink}`);
+favouriteDrink = "redWine";
+
+console.log(`My favouriteDrink is now ${favouriteDrink} `);
+favouriteDrink = "guinness";
+let age = 19;
+let country = "UK";
+if (age > 17 && country == "UK") { 
+    console.log("You are old enough and in the right country.");
+}
+else  {
+    console.log("You are either too young or in the wrong country.")}
+    let space1 = "X"
+let space2 = "O"
+let space3 = " "
+let space4 = "X"
+let space5 = "X"
+let space6 = " "
+let space7 = "O"
+let space8 = " "
+let space9 = " "
+console.log(`${space1} | ${space2} | ${space3}`);
+console.log("---------");
+console.log(`${space4} | ${space5} | ${space6}`);
+console.log("---------");
+console.log(`${space7} | ${space8} | ${space9}`);
+var horiz = "-----------------------"
+var blank = "       |       |     "
+var s1 = "   O   "
+var s2 = "   X   "
+var s3 = "   X   "
+var s4 = "   X   "
+var s5 = "   O   "
+var s6 = "   O   "
+var s7 = "   X   "
+var s8 = "   O   "
+var s9 = "   X   "
+console.log(" ");
+console.log(blank)
+console.log(`${s1}|${s2}|${s3}`)
+console.log(blank);
+console.log(horiz);
+console.log(blank)
+console.log(`${s4}|${s5}|${s6}`)
+console.log(blank);
+console.log(horiz);
+console.log(blank)
+console.log(`${s7}|${s8}|${s9}`)
+console.log(blank)
